8f7dc5de8e8cfe3b96ffaa9b8c8df20abc0f8e27
JotaDF <j.wilson.df@gmail.com>
2026-07-24 11:39:08 -0300

    Update ManterRelatorio.php

--- actions/ManterRelatorio.php
+++ actions/ManterRelatorio.php
@@ -12,7 +12,7 @@ class ManterRelatorio extends Model {
      * Retorna todos os registros e colunas da tabela de Selagem de Lotes
      */
     public function listarSelagem($filtro = ''){
-        $sql = "SELECT * FROM selagem_lotes $filtro ORDER BY data_formulario DESC";
+        $sql = "SELECT * FROM selagem_lotes_import $filtro ORDER BY data_formulario DESC";
         $resultado = $this->db->Execute($sql);
         $array_dados = array();
         
@@ -30,7 +30,7 @@ class ManterRelatorio extends Model {
      * Busca uma Selagem de Lote específica pelo ID da Submissão
      */
     public function getSelagemPorId($id_submissao) {
-        $sql = "SELECT * FROM selagem_lotes WHERE id_submissao = " . (int)$id_submissao;
+        $sql = "SELECT * FROM selagem_lotes_import WHERE id_submissao = " . (int)$id_submissao;
         $resultado = $this->db->Execute($sql);
         
         $dados = new stdClass();
@@ -47,9 +47,9 @@ class ManterRelatorio extends Model {
      */
     public function listarDomicilios($filtro = '') {
 
-        $sql = "SELECT * FROM domicilios $filtro ORDER BY numero_selo ASC";
+        $sql = "SELECT * FROM domicilios_import $filtro ORDER BY numero_selo ASC";
         if ($filtro == '') {
-            $sql = "SELECT d.*, s.data_formulario FROM domicilios as d, selagem_lotes as s WHERE d.id_submissao_pai = s.id_submissao ORDER BY d.numero_selo ASC";
+            $sql = "SELECT d.*, s.data_formulario FROM domicilios_import as d, selagem_lotes_import as s WHERE d.id_submissao_pai = s.id_submissao ORDER BY d.numero_selo ASC";
         }
         
         $resultado = $this->db->Execute($sql);
@@ -69,7 +69,7 @@ class ManterRelatorio extends Model {
      * Busca um Domicílio específico pelo Número do Selo (Chave Primária)
      */
     public function getDomicilioPorSelo($numero_selo) {
-        $sql = "SELECT * FROM domicilios WHERE numero_selo = '" . $numero_selo . "'";
+        $sql = "SELECT * FROM domicilios_import WHERE numero_selo = '" . $numero_selo . "'";
         $resultado = $this->db->Execute($sql);
         
         $dados = new stdClass();
@@ -85,7 +85,7 @@ class ManterRelatorio extends Model {
      * Busca um Domicílio pelo ID de submissão do Lote Pai (Selagem)
      */
     public function getDomicilioPorSubmissaoPai($id_submissao_pai) {
-        $sql = "SELECT * FROM domicilios WHERE id_submissao_pai = " . (int)$id_submissao_pai . " LIMIT 1";
+        $sql = "SELECT * FROM domicilios_import WHERE id_submissao_pai = " . (int)$id_submissao_pai . " LIMIT 1";
         $resultado = $this->db->Execute($sql);
         
         $dados = new stdClass();
@@ -101,7 +101,7 @@ class ManterRelatorio extends Model {
      * Retorna todos os registros e colunas da tabela de Cadastro Sociojurídico
      */
     public function listarSociojuridico($filtro = '') {
-        $sql = "SELECT * FROM cadastro_sociojuridico  $filtro ORDER BY r1_nome ASC";
+        $sql = "SELECT * FROM cadastro_sociojuridico_import  $filtro ORDER BY r1_nome ASC";
         $resultado = $this->db->Execute($sql);
         $array_dados = array();
         
@@ -119,7 +119,7 @@ class ManterRelatorio extends Model {
      * Busca o Cadastro Sociojurídico de uma família pelo ID da Submissão
      */
     public function getSociojuridicoPorId($id_submissao) {
-        $sql = "SELECT * FROM cadastro_sociojuridico WHERE id_submissao = " . (int)$id_submissao;
+        $sql = "SELECT * FROM cadastro_sociojuridico_import WHERE id_submissao = " . (int)$id_submissao;
         $resultado = $this->db->Execute($sql);
         
         $dados = new stdClass();
@@ -136,7 +136,7 @@ class ManterRelatorio extends Model {
      */
     public function getSociojuridicoPorCodigoSelo($codigo_selo) {
         // Protege a string simples com escape básico
-        $sql = "SELECT * FROM cadastro_sociojuridico WHERE codigo_selo = '" . $codigo_selo . "' LIMIT 1";
+        $sql = "SELECT * FROM cadastro_sociojuridico_import WHERE codigo_selo = '" . $codigo_selo . "' LIMIT 1";
         $resultado = $this->db->Execute($sql);
         
         $dados = new stdClass();
@@ -154,7 +154,7 @@ class ManterRelatorio extends Model {
      */
     public function listarCaracterizacao($filtro = '') {
         // 🔥 Alterado para buscar todas as colunas de forma irrestrita
-        $sql = "SELECT * FROM caracterizacao_vulnerabilidade $filtro ORDER BY data_registro DESC";
+        $sql = "SELECT * FROM caracterizacao_vulnerabilidade_import $filtro ORDER BY data_registro DESC";
                 
         $resultado = $this->db->Execute($sql);
         $array_dados = array();
@@ -176,7 +176,7 @@ class ManterRelatorio extends Model {
      * Busca um registro completo através do ID de Submissão (Chave Primária)
      */
     public function getCaracterizacaoPorIdSubmissao($id_submissao) {
-        $sql = "SELECT * FROM caracterizacao_vulnerabilidade WHERE id_submissao = " . (int)$id_submissao;
+        $sql = "SELECT * FROM caracterizacao_vulnerabilidade_import WHERE id_submissao = " . (int)$id_submissao;
         $resultado = $this->db->Execute($sql);
         
         $dados = new stdClass();
@@ -192,7 +192,7 @@ class ManterRelatorio extends Model {
      * Busca um registro através da Chave Estrangeira (Código do Selo)
      */
     public function getCaracterizacaoPorCodigoSelo($codigo_selo) {
-        $sql = "SELECT * FROM caracterizacao_vulnerabilidade WHERE codigo_selo = '" . $codigo_selo . "'";
+        $sql = "SELECT * FROM caracterizacao_vulnerabilidade_import WHERE codigo_selo = '" . $codigo_selo . "'";
         $resultado = $this->db->Execute($sql);
         
         $dados = new stdClass();
