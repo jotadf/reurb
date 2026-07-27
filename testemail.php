507d18b4193008f5fb590227f55337d13b7ccc4e
JotaDF <j.wilson.df@gmail.com>
2026-07-27 11:15:05 -0300

    Update testemail.php

--- testemail.php
+++ testemail.php
@@ -1,36 +1,23 @@
 <?php
 //-------------------------------------------------------------------------------------------
-$emailsender='no-replay@jotadf.com.br'; // Substitua essa linha pelo seu e-mail@seudominio
-$nomeremetente = 'Jota';//nome do remetente
-$emailremetente = 'jotakakashidf@gmail.com';//email do remetente
-$emaildestinatario = 'j.wilson.df@gmail.com';//email destinatario
-$comcopia = '';//com copia se precisar
-$comcopiaoculta = '';//com copia oculta se precisar
-$assunto = 'assunto';
-$mensagem = 'mensagem';
-$mensagemHTML = 'mensagem html do corpo';
+    $headers = [
+        'HTTP_CF_CONNECTING_IP',
+        'HTTP_X_REAL_IP',
+        'HTTP_X_FORWARDED_FOR',
+        'REMOTE_ADDR'
+    ];
 
-//----------------------------
-/* Montando o cabeçalho da mensagem */
-$quebra_linha = '\n';
-$headers = 'MIME-Version: 1.1' .$quebra_linha;
-$headers .= 'Content-type: text/html; charset=UTF-8' .$quebra_linha;
-// Perceba que a linha acima contem 'text/html', sem essa linha, a mensagem nao chegara formatada.
-$headers .= 'From: ' . $emailsender.$quebra_linha;
-$headers .= 'Cc: ' . $comcopia . $quebra_linha;
-$headers .= 'Bcc: ' . $comcopiaoculta . $quebra_linha;
-$headers .= 'Reply-To: ' . $emailremetente . $quebra_linha;
-// Note que o e-mail do remetente sera usado no campo Reply-To (Responder Para)
+    foreach ($headers as $header) {
+        if (!empty($_SERVER[$header])) {
 
-/* Enviando a mensagem */
+            $ips = explode(',', $_SERVER[$header]);
 
-//obrigatorio o uso do parametro -r (concatenacao do 'From na linha de envio'), aqui na Locaweb:
-
-if(!mail($emaildestinatario, $assunto, $mensagemHTML, $headers ,'-r'.$emailsender)){ // Se for Postfix
-$headers .= 'Return-Path: ' . $emailsender . $quebra_linha; // Se 'nao for Postfix'
-mail($emaildestinatario, $assunto, $mensagemHTML, $headers );
-}
-
-/* FIM */
-
-// }
+            foreach ($ips as $ip) {
+                $ip = trim($ip);
+                echo $ip . "<br>";
+                if (filter_var($ip, FILTER_VALIDATE_IP)) {
+                    echo " Validado: " . $ip . "<br>";
+                }
+            }
+        }
+    }
