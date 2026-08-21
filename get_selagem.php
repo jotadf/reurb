a50eea4da80dbb11e46f68928a9bd21d49a7221c
JotaDF <j.wilson.df@gmail.com>
2026-08-21 14:38:13 -0300

    ajustes

--- get_selagem.php
+++ get_selagem.php
@@ -12,7 +12,11 @@
             echo "  <td>".$obj->data_formulario."</td>";
             $btn_excluir = "&nbsp;&nbsp;<button class='btn btn-secondary btn-sm' type='button' title='Possuí dependências!'><i class='far fa-trash-alt' alt='Possuí dependências!'></i></button>";
             $btn_editar = "<a class='btn btn-primary btn-sm' type='button' href='editar_selagem.php?id=".$obj->id_submissao."'><i class='fas fa-edit'></i></a>";
-            $btn_domicilios = "<a class='btn btn-info btn-sm' type='button' href='editar_domicilio.php?id_selagem=".$obj->id_submissao."'><i class='fas fa-home'></i></a>";
+            $btn_domicilios = "<button class='btn btn-danger btn-sm' type='button' title='Selegem sem domicílio'><i class='fas fa-home'></i></button>";
+            if($manterRelatorio->getDomicilioPorSubmissaoPai($obj->id_submissao)->id_submissao_pai > 0){
+                $btn_domicilios = "<a class='btn btn-info btn-sm' type='button' href='editar_domicilio.php?id_selagem=".$obj->id_submissao."'><i class='fas fa-home'></i></a>";
+            }
+
             if($obj->excluir){
                 $btn_excluir = "&nbsp;&nbsp;<button class='btn btn-danger btn-sm' type='button' onclick='excluir(".$obj->id_submissao.",\"".$obj->uuid."\")'><i class='far fa-trash-alt'></i></button>";
             }
--- sociojuridicos.php
+++ sociojuridicos.php
@@ -75,7 +75,7 @@ and open the template in the editor.
 
                     <div class="container-fluid">
                         <!-- Project Card Example -->
-                        <div class="card mb-4 border-primary" style="max-width:900px">
+                        <div class="card mb-4 border-primary" style="max-width:1200px">
                             <div class="row ml-0 card-header py-2 bg-gradient-primary" style="width:100%">
                                 <div class="col-sm ml-0" style="max-width:50px;">
                                     <i class="fa fa-home fa-2x text-white"></i> 
