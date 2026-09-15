de22dd91d98bed463fde302b8a1ca3345c8c59fe
JotaDF <j.wilson.df@gmail.com>
2026-09-15 18:43:22 -0300

    Update ManterRelatorio.php

--- actions/ManterRelatorio.php
+++ actions/ManterRelatorio.php
@@ -94,7 +94,7 @@ class ManterRelatorio extends Model {
     }
     public function alteraSeloSociojuridico($selo, $novo_selo, $id_submissao, $id_usuario) {
         $sql = "UPDATE cadastro_sociojuridico_import SET codigo_selo = '{$novo_selo}' WHERE id_submissao = " . $id_submissao;
-        $sql_log = "INSERT INTO auditoria (tabela, acao, usuario, valor_antigo, valor_novo, atualizado) 
+        $sql_log = "INSERT INTO auditoria (tabela, acao, usuario, valor_antigo, valor_atual, atualizado) 
     VALUES ('cadastro_sociojuridico_import', 'UPDATE', '{$id_usuario}', '{$selo}', '{$novo_selo}', now())";
 
         $resultado = $this->db->Execute($sql);
